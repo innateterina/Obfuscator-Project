c6c24b8c538fd255143ab3a9d3c2a07bd4bd54df
innateterina <innacharykova@gmail.com>
2024-09-26 17:18:04 +0100

    add function for parquet format and main_obfuscator function that checks the format of the file and invoke the suitable function

--- main/main.py
+++ main/main.py
@@ -18,10 +18,22 @@
 # ...
 # 1234,'***','Software','2024-03-31','***'
 import boto3
+import pandas as pd
 import csv
 import json
 from io import StringIO
 from helpers import parse_s3_location, replace_pii_csv_data, replace_pii_json_data
+
+
+def main_obfuscator(s3_location: str, pii_fields: list):
+    if s3_location.endswith('.csv'):
+        return obfusc_csv(s3_location, pii_fields)
+    elif s3_location.endswith('.json'):
+        return obfusc_json(s3_location, pii_fields)
+    elif s3_location.endswith('.parquet'):
+        return obfusc_parquet(s3_location, pii_fields)
+    else:
+        raise ValueError("Unsupported file format.")
 
 
 def obfusc_json(s3_location: str, pii_fields: list):
@@ -51,10 +63,15 @@ def obfusc_csv(s3_location: str,  pii_fields: list):
     return csv_output.getvalue().encode('utf-8')
 
 
-# input_json = {
-#     "file_to_obfuscate": "s3://my_ingestion_bucket/new_data/file1.csv",
-#     "pii_fields": ["name", "email_address"]
-# }
+def obfusc_parquet(s3_location: str, pii_fields: list):
+    s3 = boto3.client('s3')
+    bucket_name, file_key = parse_s3_location(s3_location)
+    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+    data = pd.read_parquet(BytesIO(obj['Body'].read()))
+    for field in pii_fields:
+        if field in data.columns:
+            data[field] = '***'
 
-# obfusc_data = obfusc_csv(
-#     input_json['file_to_obfuscate'], input_json['pii_fields'])
+    output = BytesIO()
+    data.to_parquet(output, index=False)
+    return output.getvalue()
